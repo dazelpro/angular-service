128fbfa22d88e625e26e8ec98fd33f3344b216c8
dazelpro <dazelpro@gmail.com>
2020-10-07 21:08:55 +0700

    + module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+
+import { HttpClientModule } from '@angular/common/http'; // tambah baris ini
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule // tambah baris ini
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
